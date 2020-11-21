a4b7434993cd5163b0d30041fb6afda2f787b764
Renato <renato.silva.an@gmail.com>
2020-11-21 00:28:30 +0000

    New readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# preco-operadoras
+# Preço Operadoras
+
+Monitorização dos preços das operadoras para um pacote básico de de TV+NET+TELEFONE.
+
+Disponível em: ....
--- backend/api/controllers/pricesController.js
+++ backend/api/controllers/pricesController.js
@@ -6,6 +6,9 @@ module.exports = () => {
   var MongoClient = require('mongodb').MongoClient;
   var url = "mongodb://localhost:27017/";
 
+  function getMoment(obj) {
+      return [obj.timestamp, obj.value]
+  }
 
   controller.getPrices = (req, res) => {
       MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
@@ -16,16 +19,41 @@ module.exports = () => {
 
         dbo.collection("pricedata").find({}, [{'operator':false,'value':true,'timestamp':true}]).project({_id:0}).toArray(function(err, result) {
           if (err) throw err;
+
+          var meoData = [];
+          var nosData = [];
+          var vodafoneData = [];
+
+
+          meoResult = result.filter(function (e) {
+                    return e.operator == 1;
+                });
+
+          meoResult.forEach(function(entry) {
+                meoData.push( getMoment(entry) );
+            });
+
+
+          nosResult = result.filter(function (e) {
+                  return e.operator == 2;
+              });
+
+          nosResult.forEach(function(entry) {
+              nosData.push( getMoment(entry) );
+          });
+
+          vodafoneResult = result.filter(function (e) {
+                  return e.operator == 2;
+              });
+
+          vodafoneResult.forEach(function(entry) {
+              vodafoneData.push( getMoment(entry) );
+          });
+
           data = {
-              meo : result.filter(function (e) {
-                        return e.operator == 1;
-                    }),
-              nos : result.filter(function (e) {
-                        return e.operator == 2;
-                    }),
-              vodafone : result.filter(function (e) {
-                        return e.operator == 3;
-                    }),
+              meo : meoData,
+              nos : nosData,
+              vodafone : vodafoneData,
           };
 
           res.status(200).json(data)
