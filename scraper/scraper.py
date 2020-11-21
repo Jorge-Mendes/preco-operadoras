8cdfa88a28485b4ce583567b0a691cd3e15227c9
Renato <renato.silva.an@gmail.com>
2020-11-21 13:05:54 +0000

    Remove use of selenium

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -11,7 +11,7 @@ import re
 
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+    'User-Agent': '"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
 }
 
 
