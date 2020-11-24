51c2cd5686b36f286e6b7eab55d6c5a178022d2b
Renato <renato.silva.an@gmail.com>
2020-11-24 13:49:10 +0000

    Add screenshots

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -142,11 +142,11 @@ driver = webdriver.Firefox(options=options,firefox_profile=fp, executable_path=G
 driver.set_window_size(1080, 1920)
 
 print('Getting MEO screenshot')
-get_screenshot('https://www.meo.pt/servicos/casa/fibra/pacotes-tv-net-voz', 'screenshots/meo/meo_'+str(timestamp)+'.png')
+#get_screenshot('https://www.meo.pt/servicos/casa/fibra/pacotes-tv-net-voz', 'screenshots/meo/meo_'+str(timestamp)+'.png')
 print('Getting VODAFONE screenshot')
-get_screenshot('https://www.vodafone.pt/pacotes.html#3p', 'screenshots/vodafone/vodafone_'+str(timestamp)+'.png')
+#get_screenshot('https://www.vodafone.pt/pacotes.html#3p', 'screenshots/vodafone/vodafone_'+str(timestamp)+'.png')
 print('Getting NOS screenshot')
-get_screenshot('https://www.nos.pt/particulares/pacotes/todos-os-pacotes/Paginas/pacotes.aspx?source=menupacotes&content=topo', 'screenshots/nos/nos_'+str(timestamp)+'.png')
+#get_screenshot('https://www.nos.pt/particulares/pacotes/todos-os-pacotes/Paginas/pacotes.aspx?source=menupacotes&content=topo', 'screenshots/nos/nos_'+str(timestamp)+'.png')
 
 driver.close()
 driver.quit()
